43dc1b8c44d828c2498e4006829f85eac94b0273
Jonathan Tredway <jptredway@gmail.com>
2019-02-18 10:30:19 -0600

    Add recursive approach to removeDuplicates problem and update test suite

--- LinkedLists/removeDuplicates/index.js
+++ LinkedLists/removeDuplicates/index.js
@@ -1,4 +1,15 @@
 // Remove duplicate nodes from an unsorted singly-linked list
+
+// Recursive
+const removeDuplicatesRecursive = (node, seen = new Set()) => {
+  if (!node) return null;
+  if (seen.has(node.val)) return removeDuplicatesRecursive(node.next, seen);
+  seen.add(node.val);
+  node.next = removeDuplicatesRecursive(node.next, seen);
+  return node;
+};
+
+// Iterative
 const removeDuplicates = head => {
   if (!head || !head.next) return head;
   const seen = new Set();
@@ -17,4 +28,4 @@ const removeDuplicates = head => {
   return head;
 };
 
-module.exports = removeDuplicates;
+module.exports = { removeDuplicates, removeDuplicatesRecursive };
--- LinkedLists/removeDuplicates/test.js
+++ LinkedLists/removeDuplicates/test.js
@@ -1,27 +1,73 @@
 const expect = require("chai").expect;
 const LinkedList = require("../LinkedList");
-const removeDuplicates = require("./index");
+const { removeDuplicates, removeDuplicatesRecursive } = require("./index");
 
 describe("Remove duplicates from a singly-linked list", function() {
-  it("should remove duplicates", function() {
-    const List1 = new LinkedList(1);
-    List1.addToTail(2);
-    List1.addToTail(0);
-    List1.addToTail(1);
-    List1.addToTail(5);
-    removeDuplicates(List1.head);
-    const List2 = new LinkedList(1);
-    List2.addToTail(2);
-    List2.addToTail(0);
-    List2.addToTail(5);
-    expect(List1.head).to.deep.equal(List2.head);
+  describe("Iterative Approach", function() {
+    it("should remove duplicates", function() {
+      const List1 = new LinkedList(1);
+      List1.addToTail(2);
+      List1.addToTail(0);
+      List1.addToTail(1);
+      List1.addToTail(5);
+      removeDuplicates(List1.head);
+
+      const List2 = new LinkedList(1);
+      List2.addToTail(2);
+      List2.addToTail(0);
+      List2.addToTail(5);
+      removeDuplicates(List2.head);
+      expect(List1.head).to.deep.equal(List2.head);
+
+      const List3 = new LinkedList(2);
+      List1.addToTail(2);
+      List1.addToTail(2);
+      List1.addToTail(2);
+      List1.addToTail(2);
+      removeDuplicates(List3.head);
+      expect(List3.head).to.deep.equal({ val: 2, next: null });
+    });
+
+    it("should modify the given Linked List", function() {
+      const List1 = new LinkedList(1);
+      List1.addToTail(1);
+      expect(List1.head.next).to.deep.equal({ val: 1, next: null });
+      removeDuplicates(List1.head);
+      expect(List1.head.next).to.equal(null);
+    });
   });
 
-  it("should modify the given Linked List", function() {
-    const List1 = new LinkedList(1);
-    List1.addToTail(1);
-    expect(List1.head.next).to.deep.equal({ val: 1, next: null });
-    removeDuplicates(List1.head);
-    expect(List1.head.next).to.equal(null);
+  describe("Recursive Approach", function() {
+    it("should remove duplicates", function() {
+      const List1 = new LinkedList(1);
+      List1.addToTail(2);
+      List1.addToTail(0);
+      List1.addToTail(1);
+      List1.addToTail(5);
+      removeDuplicatesRecursive(List1.head);
+
+      const List2 = new LinkedList(1);
+      List2.addToTail(2);
+      List2.addToTail(0);
+      List2.addToTail(5);
+      removeDuplicatesRecursive(List2.head);
+      expect(List1.head).to.deep.equal(List2.head);
+
+      const List3 = new LinkedList(2);
+      List1.addToTail(2);
+      List1.addToTail(2);
+      List1.addToTail(2);
+      List1.addToTail(2);
+      removeDuplicatesRecursive(List3.head);
+      expect(List3.head).to.deep.equal({ val: 2, next: null });
+    });
+
+    it("should modify the given Linked List", function() {
+      const List1 = new LinkedList(1);
+      List1.addToTail(1);
+      expect(List1.head.next).to.deep.equal({ val: 1, next: null });
+      removeDuplicatesRecursive(List1.head);
+      expect(List1.head.next).to.equal(null);
+    });
   });
 });
